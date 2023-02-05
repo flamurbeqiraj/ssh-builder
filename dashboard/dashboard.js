1a807ace6bf78ab47652fb6b32ecd380dfbe34bd
Flamur MacBook <flamurbeqiraj95@gmail.com>
2023-02-05 17:37:08 +0100

    Added dashboard page & improved register new project page

---
+++ dashboard/dashboard.js
@@ -0,0 +1,37 @@
+const project_list = localStorage.getItem("project_list");
+const table_list = document.querySelector("#list");
+const add_project = document.querySelector("#add_project");
+
+if (project_list !== null) {
+    let list = JSON.parse(project_list);
+    console.log("list", list);
+    if (list.length > 0) {
+        table_list.innerHTML = "";
+        let i = 1;
+        for (let item of list) {
+            table_list.innerHTML += 
+            `<tr>
+                <td>${i}</td>
+                <td><i class="bi bi-play-circle-fill text-success cursor-pointer"></i></td>
+                <td>${item.project_name}</td>
+                <td>03.02.2023 15:30</td>
+                <td>Succeed!</td>
+            </tr>`;
+            i++;
+        }
+    } else {
+        table_list.innerHTML = 
+            `<tr>
+                <td colspan="4" class="text-center"><i>Empty list</i></td>
+            </tr>`;
+    }
+} else {
+    table_list.innerHTML = 
+    `<tr>
+        <td colspan="4" class="text-center"><i>Register your first project</i></td>
+    </tr>`;
+}
+
+add_project.addEventListener("click", () => {
+    window.location.href = "../register/register.html";
+})
---
+++ register/register.js
@@ -0,0 +1,59 @@
+const project_list = localStorage.getItem("project_list");
+const project_name = document.getElementById('project_name')
+const localrepo = document.getElementById('localrepo')
+const dist = document.getElementById('dist')
+const remote_url = document.getElementById('remote_url')
+const build_param = document.getElementById('build_param')
+const register = document.getElementById('register')
+const go_back = document.getElementById('go_back')
+
+register.addEventListener('click', async () => {
+    let error_list = [];
+    if (project_name.value.length < 3) {
+        error_list.push(error_list.length+1+". Your project name should have more than 3 chars!")
+    }
+    if (localrepo.innerText === undefined || localrepo.innerText === "" || localrepo.innerText === "Not choosen") {
+        error_list.push(error_list.length+1+". Please select a repository on you machine!")
+    }
+    if (dist.innerText === undefined || dist.innerText === "" || dist.innerText === "Not choosen") {
+        error_list.push(error_list.length+1+". Please select destribution path of your app!")
+    }
+    if (remote_url.value === "" || remote_url.value.length < 7) {
+        error_list.push(error_list.length+1+". Specify where the dist files should be moved!")
+    }
+
+    if (error_list.length === 0) {
+        let new_list = JSON.parse(project_list);
+        let item = {
+            project_name: project_name.value,
+            local_repo: localrepo.innerText,
+            dist: dist.innerText,
+            remote_url: remote_url.value,
+            build_param: build_param.value
+        };
+        if (new_list === null) {
+            new_list = [item];
+        } else {
+            new_list.push(item);
+        }
+        localStorage.setItem("project_list", JSON.stringify(new_list))
+        alert("Your project has been added successfuly!");
+        window.location.href = "../dashboard/dashboard.html"
+    } else {
+        alert(error_list.join("\n"));
+    }
+})
+
+localrepo.addEventListener('click', async () => {
+    const filePath = await window.electronAPI.openFile()
+    localrepo.innerText = filePath;
+})
+
+dist.addEventListener('click', async () => {
+    const filePath = await window.electronAPI.openFile()
+    dist.innerText = filePath;
+})
+
+go_back.addEventListener('click', async () => {
+    window.location.href = "../dashboard/dashboard.html";
+})
