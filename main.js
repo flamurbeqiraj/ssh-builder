e1bf21b3a291ce6f7253bed95f9f6fb2e5e88fd1
Flamur MacBook <flamurbeqiraj95@gmail.com>
2023-02-05 17:37:30 +0100

    Restructure of files

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog, ipcMain } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 950,
     height: 600,
     webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
-      contextIsolation: false,
       enableRemoteModule: true
     },
   })
   win.removeMenu();
-  win.loadFile('src/index.html')
+
+  win.loadFile('dashboard/dashboard.html')
+
   win.webContents.openDevTools()
+  ipcMain.handle('dialog:openFile', handleFileOpen);
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 }
 
 app.whenReady().then(() => {
   createWindow()
 })
+
+async function handleFileOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog({ properties: ['openDirectory'] }, (directory) => {
+    // console.log(directory)
+  })
+  if (canceled) {
+    return
+  } else {
+    return filePaths[0]
+  }
+}
